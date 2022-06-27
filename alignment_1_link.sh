87f87c2c11593ce15c97fdccd68be0536899a1a3
keishi02 <70524669+keishi02@users.noreply.github.com>
2022-06-27 10:58:24 -0400

    Update alignment_1_link.sh

--- alignment_1_link.sh
+++ alignment_1_link.sh
@@ -10,12 +10,9 @@
 
 #load modules
 module load bioinfo
-module load samtools
 
 wget $1 -O /depot/bharpur/data/projects/keishi/fastq_files/"$2".fastq.gz
 
-/depot/bharpur/apps/NextGenMap-0.5.2/bin/ngm-0.5.2/ngm -r /depot/bharpur/data/ref_genomes/AMEL/Amel_HAv3.1_genomic.fna -q /depot/bharpur/data/projects/keishi/fastq_files/"$2".fastq.gz -o /depot/bharpur/data/projects/keishi/sam_files/"$2".sam
-samtools view -bT /depot/bharpur/data/ref_genomes/AMEL/Amel_HAv3.1_genomic.fna /depot/bharpur/data/projects/keishi/sam_files/"$2".sam > /depot/bharpur/data/projects/keishi/bam_files/"$2".bam
+/depot/bharpur/apps/NextGenMap-0.5.2/bin/ngm-0.5.2/ngm -r /depot/bharpur/data/ref_genomes/AMEL/Amel_HAv3.1_genomic.fna -q /depot/bharpur/data/projects/keishi/fastq_files/"$2".fastq.gz -b -o /depot/bharpur/data/projects/keishi/bam_files/"$2".bam
 
-rm /depot/bharpur/data/projects/keishi/sam_files/"$2".sam
 rm /depot/bharpur/data/projects/keishi/fastq_files/"$2".fastq.gz
