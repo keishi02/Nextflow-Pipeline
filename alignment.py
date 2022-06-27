64ce832af69f5ad4da8fbed1d51da79f69f56402
keishi02 <70524669+keishi02@users.noreply.github.com>
2022-06-27 11:07:57 -0400

    Update alignment.py

--- alignment.py
+++ alignment.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import subprocess
-import shlex
 import sys
 
 #var1: how many rows of the genomic dataset to process OR "all" for all rows
@@ -9,7 +8,7 @@ df = pd.read_csv("/depot/bharpur/data/projects/keishi/genomics_dataset.csv")
 df_mellifera = df[df["tax_id"] == 7460]
 
 if (not sys.argv[1] == "all"):
-    df_mellifera = df_mellifera.head(sys.argv[1])
+    df_mellifera = df_mellifera.head(int(sys.argv[1]))
 
 
 df_mellifera1 = df_mellifera[df_mellifera["fastq_ftp"].str.contains(";")]
@@ -30,7 +29,7 @@ for i in range(len(df_mellifera1)):
     var3 = df_mellifera1["run_accession"].values[i]
     
     #call shell script
-    subprocess.call(shlex.split('depot/bharpur/data/projects/keishi/alignment_2_links.sh {var1} {var2} {var3}'))
+    subprocess.call(['/depot/bharpur/data/projects/keishi/alignment_2_links.sh', var1, var2, var3])
 
 
 #for all samples with 1 ftp link  
@@ -43,7 +42,7 @@ for i in range(len(df_mellifera2)):
     var2 = df_mellifera2["run_accession"].values[i]
     
     #call shell script
-    subprocess.call(shlex.split('depot/bharpur/data/projects/keishi/alignment_1_link.sh {var1} {var2}'))
+    subprocess.call(['/depot/bharpur/data/projects/keishi/alignment_1_link.sh', var1, var2])
     
     
     
